0668197abc8bae41ef35a0df2fb41d01760c2f31
Matt Kornfield <kornfield@gretel.ai>
2024-09-30 15:33:07 -0700

    [TASK] Redeploy hybrid Azure dev and serverless hybrid in same cluster
    Co-authored-by: Ben McCown <ben@gretel.ai>
    GitOrigin-RevId: d40e52b1e8e2415bb10575ab0b5f5ad33516efd2

--- terraform-v2/azure/examples/full_deployment/outputs.tf
+++ terraform-v2/azure/examples/full_deployment/outputs.tf
@@ -1,3 +1,11 @@
+output "resource_group_name" {
+  value = module.prereqs.hybrid_resource_group_name
+}
+
+output "kubernetes_oidc_issuer_url" {
+  value = module.cluster.kubernetes_oidc_issuer_url
+}
+
 output "key_vault_key_name" {
   value = module.gretel_hybrid.key_vault_key_name
 }
@@ -9,6 +17,34 @@ output "key_vault_id" {
   value = module.gretel_hybrid.key_vault_id
 }
 
+output "key_vault_public_key_pem" {
+  value = module.gretel_hybrid.key_vault_public_key_pem
+}
+
 output "storage_account_id" {
   value = module.gretel_hybrid.storage_account_id
+}
+
+output "storage_account_name" {
+  value = module.gretel_hybrid.storage_account_name
+}
+
+output "storage_sink_container" {
+  value = module.gretel_hybrid.storage_sink_container
+}
+
+output "workflow_worker_client_id" {
+  value = module.gretel_hybrid.workflow_worker_client_id
+}
+
+output "model_worker_client_id" {
+  value = module.gretel_hybrid.model_worker_client_id
+}
+
+output "workflow_worker_id" {
+  value = module.gretel_hybrid.workflow_worker_id
+}
+
+output "model_worker_id" {
+  value = module.gretel_hybrid.model_worker_id
 }
--- terraform-v2/azure/modules/gretel_hybrid/outputs.tf
+++ terraform-v2/azure/modules/gretel_hybrid/outputs.tf
@@ -9,6 +9,34 @@ output "key_vault_id" {
   value = azurerm_key_vault.gretel_key_vault.id
 }
 
+output "key_vault_public_key_pem" {
+  value = azurerm_key_vault_key.gretel_key.public_key_pem
+}
+
 output "storage_account_id" {
   value = azurerm_storage_account.gretel_hybrid.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.gretel_hybrid.name
+}
+
+output "storage_sink_container" {
+  value = azurerm_storage_container.sink.name
+}
+
+output "workflow_worker_client_id" {
+  value = azurerm_user_assigned_identity.gretel_workflow_worker.client_id
+}
+
+output "model_worker_client_id" {
+  value = azurerm_user_assigned_identity.gretel_model_worker.client_id
+}
+
+output "workflow_worker_id" {
+  value = azurerm_user_assigned_identity.gretel_workflow_worker.id
+}
+
+output "model_worker_id" {
+  value = azurerm_user_assigned_identity.gretel_model_worker.id
 }
